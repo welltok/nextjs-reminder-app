eb57333d37dec09283a6d24cc81825f95c773429
Jonathan Rodriguez <clubzspadez@gmail.com>
2025-02-20 12:13:19 -0500

    navbar seperation

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,43 +3,12 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Navbar from "@/components/NavBar";
 
 export default function UsersPage(): JSX.Element {
   return (
-    <div className="container-fluid p-0" style={{ backgroundColor: "#f8f9fa" }}>
-      {/* Top Navbar */}
-      <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom px-3">
-        {/* Logo + Brand */}
-        <Link href="/" className="navbar-brand d-flex align-items-center">
-          <Image
-            src="/personify_logo.png"
-            alt="Personify Health Logo"
-            width={150}
-            height={40}
-          />
-        </Link>
-
-        {/* Navigation Links */}
-        <div className="ms-auto">
-          <ul className="navbar-nav flex-row">
-            <li className="nav-item me-3">
-              <Link href="/" className="nav-link active">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item me-3">
-              <Link href="/users" className="nav-link">
-                Users
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/logout" className="nav-link">
-                Log Out
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <div>
+      <Navbar />
     </div>
   );
 }
---
+++ components/NavBar.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar(): JSX.Element {
+  return (
+    <div className="container-fluid p-0" style={{ backgroundColor: "#f8f9fa" }}>
+      {/* Top Navbar */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom px-3">
+        {/* Logo + Brand */}
+        <Link href="/" className="navbar-brand d-flex align-items-center">
+          <Image
+            src="/personify_logo.png"
+            alt="Personify Health Logo"
+            width={150}
+            height={40}
+          />
+        </Link>
+
+        {/* Navigation Links */}
+        <div className="ms-auto">
+          <ul className="navbar-nav flex-row">
+            <li className="nav-item me-3">
+              <Link href="/" className="nav-link active">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item me-3">
+              <Link href="/users" className="nav-link">
+                Users
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/logout" className="nav-link">
+                Log Out
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
